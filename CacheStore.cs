5897f68df2c14a1dc892179ef32a6349a1adfd73
Chris Doty (Civic) <dotyc@civicinc.com>
2013-08-26 09:18:13 -0500

    more namespace changes

---
+++ CacheStore.cs
@@ -0,0 +1,9 @@
+namespace Civic.Core.Caching
+{
+	public enum CacheStore
+	{
+		Application = 1,
+		Session,
+		DoNotStore
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Civic.Core.Caching")]
+[assembly: AssemblyProduct("Civic.Core.Caching")]
+[assembly: Guid("57862de0-542d-452b-86fe-9e5703af203e")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2012 Civic Engineering & IT")]
