8a864a078bcf53005d3b9b352750d256b5a2bb5c
unknown <suttona@civicinc.com>
2013-11-14 15:21:28 -0600

    Add sql provider

---
+++ Providers/SQLPRovider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace Civic.Core.Caching.Providers
+{
+    public static class SQLProvider
+    {
+        public static void WriteCache<TV>(string key, TV value, TimeSpan decay,
+            CacheStore cacheStore = CacheStore.Session)
+        {
+            try
+            {
+                Cache cache = HttpRuntime.Cache;
+                if (cache != null)
+                {
+                    cache[key] = value;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(
+                    string.Format("Error Accessing Cache Manager.\r\nKey:{0}\r\nCache Store:{1}\r\nError:{2}", key,
+                        cacheStore, ex.Message));
+            }
+        }
+
+
+        public static TV ReadCache<TV>(string key, TV nullValue, CacheStore cacheStore = CacheStore.Session)
+        {
+            Cache cache = HttpRuntime.Cache;
+            if (cache != null)
+            {
+                try
+                {
+                    //return (cache[key] == null) ? nullValue : (TV) cache[key];
+
+                    //return (TV)LoginHelper.ReadCachefromDB<TV>(key, date);
+
+                    if (cache[key] != null)
+                    {
+                        return (TV) cache[key];
+                    }
+                    return nullValue;
+                }
+                catch (Exception ex)
+                {
+                    throw new ArgumentException(
+                        string.Format("Error Accessing Cache Manager.\r\nKey:{0}\r\nCache Store:{1}\r\nError:{2}", key,
+                            cacheStore, ex.Message));
+                }
+            }
+
+            return nullValue;
+        }
+
+
+        public static void WriteCache<TV>(string key, TV value, CacheStore cacheStore = CacheStore.Session)
+        {
+            WriteCache(key, value, TimeSpan.FromHours(1), cacheStore);
+        }
+    }
+}
