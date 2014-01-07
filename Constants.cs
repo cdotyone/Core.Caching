22f2c0249a569270c021ef2da0adde2273031329
Chris Doty <dotyc@civicinc.com>
2014-01-07 13:17:20 -0600

    standardize on configuration section names

--- Constants.cs
+++ Constants.cs
@@ -3,7 +3,7 @@
     internal class Constants
     {
 		public const string CONFIG_PROP_PROVIDER = "provider";
-        public const string CORE_CACHE_SECTION = "civicCaching";
+        public const string CORE_CACHE_SECTION = "coreCaching";
 
     	public const string CONFIG_PROP_DEFAULTPROVIDER = "defaultProvider";
         public const string CONFIG_DEFAULTPROVIDER = "WebCacheProvider";
